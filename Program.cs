cfeccbdbb49c4402f5c7041d60a63ca14431bbd8
Habeebullahi01 <lawalhabeebullahi008@gmail.com>
2025-04-23 12:05:37 +0100

    feat: Swagger Docs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "Lexora Library Management Service Documntation",
+            Description = "This is the documentation for Lexora.",
+            Version = "0.1"
+        };
+        return Task.CompletedTask;
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,30 +28,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "Default");
+    options.RoutePrefix = "docs";
+});
+
+app.MapControllers();
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Lexora
+
+## Library Management Service
+
+The docs can be found at `/docs` when the API is running
